8f6c6a2118c2882f3174d2bf245bba69aa3178e7
Woody Mendoza <woody@elderlawanswers.com>
2017-02-13 13:08:43 -0500

    updates app->share to app->singleton
    Laravel 5.4 gets rid of app share

--- src/sngrl/SphinxSearch/SphinxSearchServiceProvider.php
+++ src/sngrl/SphinxSearch/SphinxSearchServiceProvider.php
@@ -7,7 +7,7 @@ class SphinxSearchServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        $this->app['sphinxsearch'] = $this->app->share(function ($app) {
+        $this->app->singleton(SphinxSearch::class, function ($app) {
             return new SphinxSearch;
         });
     }
